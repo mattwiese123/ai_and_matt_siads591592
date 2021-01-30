4c57b672a2eafbf58ba5dcf6e9405ecb22ef26fb
Ai Zhong <aizhong.ds@gmail.com>
2021-01-29 22:24:59 -0800

    add company description texts

--- plotly/app_stock_returns.py
+++ plotly/app_stock_returns.py
@@ -11,6 +11,7 @@ import plotly.express as px
 import pandas as pd
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import re
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -44,6 +45,7 @@ covid_search.index = pd.to_datetime(covid_search.index)
 
 wkly_return = spy_ret.cumsum().resample('W').sum()
 wkly_search = covid_search.resample('W').sum()
+
 spy_covid = pd.concat([wkly_return, wkly_search], axis=1)
 spy_covid.columns = ['spy_return%', 'COVID_search']
 spy_covid['spy_return%'] = spy_covid['spy_return%']*100
@@ -53,6 +55,9 @@ news_count = pd.read_csv('NYTnews_keyword_monthly.csv', index_col=0)
 news_count['date'] = '2020-'+news_count['month'].astype(str)+'-15'
 news_count['date'] = pd.to_datetime(news_count['date'])
 
+
+spy_covid_correlation_all = spy_covid.corr()['COVID_search'][0]*100
+spy_covid_correlation_after_march = spy_covid['2020-03':].corr()['COVID_search'][0]*100
 
 fig3 = make_subplots(specs=[[{"secondary_y": True}]])
 
@@ -77,15 +82,11 @@ fig3.add_trace(
 
 fig3.add_trace(
     go.Bar(x=news_count.date, y=news_count['mention_per_news'], 
-           name="NYTnews", opacity=0.3),
+           name="COVID in news", opacity=0.3),
     secondary_y=True
 )
 
-fig3.update_layout(
-    title_text="SPY Return vs COVID Search Trends in 2020"
-)
-
-fig3.update_yaxes(title_text="SPY Return%", secondary_y=False, showgrid=False)
+fig3.update_yaxes(title_text="SPY Cumulative Return%", secondary_y=False, showgrid=False)
 fig3.update_yaxes(title_text="Covid Search Trends", secondary_y=True, showgrid=False)
 
 fig3.update_xaxes(dtick="M1", showgrid=True, rangeslider_visible=True)
@@ -97,6 +98,10 @@ fig3.add_vrect(x0="2020-03-13", x1="2020-03-14", col=1,
 fig3.add_vrect(x0="2020-11-09", x1="2020-11-10", col=1,
               annotation_text="pfizer vaccine result", annotation_position="top left",
               fillcolor="green", opacity=0.5, line_width=0)
+
+# ========= Load company information data ===========
+
+comps = pd.read_csv('sp500.csv', index_col=0)
 
 
 # ==============================
@@ -113,6 +118,7 @@ app.layout = html.Div([
     html.Div(id="news"),
     html.Br(),
 
+    html.H4(children=" SPY Cumulative Return vs COVID Search Trends in 2020 (Weekly)"),
     dcc.Graph(
         id="news_searches_spy",
         figure = fig3
@@ -126,12 +132,17 @@ app.layout = html.Div([
         options=[{'label':str(i), 'value':i} for i in tickers],
         multi=True
         ),
-
+    
     html.Br(),
 
     dcc.Graph(
         id="stock_return",
         figure = fig),
+
+    html.Br(),
+
+    html.H4(children=" Company Description for Selected Tickers (from Wikipedia)"),
+    html.Div(id="company_description"),
 
     html.Br(),
     html.H4(children=" Stock Correlation with Covid Google Search Trends 2020"),
@@ -148,8 +159,6 @@ app.layout = html.Div([
     Output(component_id='news', component_property='children'),
     Input(component_id='chosen_date', component_property='value'),
 )
-
-
 def update_output_div(input_value):
 
     # return input_value
@@ -158,11 +167,11 @@ def update_output_div(input_value):
     
     return news
 
+
 @app.callback(
     Output(component_id='stock_return', component_property='figure'),
     Input(component_id='chosen_ticker', component_property='value'),
 )
-
 def update_graph(input_value):
     if not input_value:
         return None
@@ -187,6 +196,32 @@ def update_graph(input_value):
                   fillcolor="green", opacity=0.5, line_width=0)
     
     return fig
+
+
+@app.callback(
+    Output(component_id='company_description', component_property='children'),
+    Input(component_id='chosen_ticker', component_property='value'),
+)
+def update_company_info(input_value):
+    
+    input_value = list(input_value)
+    input_value = [x for x in input_value if x!='SPY']
+    if not input_value:
+        return None
+
+    comp_des = comps[comps['Symbol'].isin(input_value)]
+    texts = []
+    for i in range(len(comp_des)):
+        text = comp_des['comp_description'].iloc[i].strip().split('\n')[0]
+        t = comp_des['Symbol'].iloc[i]
+        if text.startswith('Coordinates:') and len(text.split())<20:
+            text = comp_des['comp_description'].iloc[i].strip().split('\n')[1]
+
+        text = t+': '+re.sub(r'\[\d+\]', '', text)
+        texts.append(text)
+
+    return '---------------------->'.join(texts)
+
 
 
 if __name__ == '__main__':
